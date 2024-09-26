0e020898973a1179a9fbeaade9f581c187219189
Adrian Schurz <adrian.schurz@check24.de>
2024-09-26 11:02:37 +0200

    Implement Eq instead of deriving
    in preparation of properly comparing types containing floating point fields

--- src/Types.hs
+++ src/Types.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Types
   ( Point (..),
     Vector (..),
@@ -9,11 +11,21 @@ data Point = MkPoint
     py :: Double,
     pz :: Double
   }
-  deriving stock (Eq, Show)
+  deriving stock (Show)
 
 data Vector = MkVector
   { vx :: Double,
     vy :: Double,
     vz :: Double
   }
-  deriving stock (Eq, Show)
+  deriving stock (Show)
+
+instance Eq Point where
+  (==) :: Point -> Point -> Bool
+  (MkPoint x1 y1 z1) == (MkPoint x2 y2 z2) =
+    x1 == x2 && y1 == y2 && z1 == z2
+
+instance Eq Vector where
+  (==) :: Vector -> Vector -> Bool
+  (MkVector x1 y1 z1) == (MkVector x2 y2 z2) =
+    x1 == x2 && y1 == y2 && z1 == z2
